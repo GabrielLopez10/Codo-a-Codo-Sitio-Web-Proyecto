6efaf4c141fe838594b78e78435fc166474c502f
Gaston Lopez <gastonlopez436@gmail.com>
2023-05-26 22:51:49 +0000

    Actualizacion 5

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,31 +12,124 @@
 	});});
 
 
-	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    margin:30,
-	    nav:true,
-	    autoplay:true,
-		autoplayTimeout:5000,
-		autoplayHoverPause:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        550:{
-	            items:2
-	        },
-	        750:{
-	            items:3
-	        },
-	        1000:{
-	            items:4
-	        },
-	        1200:{
-	            items:5
-	        }
-	    }
-	})
+	$(document).ready(function() {
+	    $.ajax({
+			url: 'courses.json',
+			method: 'GET',
+			dataType: 'json',
+			success: function(response) {
+				var courses = response.courses;
+
+				console.log(courses);
+
+				var courseCarousel = $('.owl-carousel');
+
+				$.each(courses, function(_index, course) {
+					var carouselItem = $('<div></div>').addClass('item').attr('id', 'courseItem_' + course.id);
+					var img = $('<img>').addClass('img').attr('src', course.image).attr('alt', course.title);
+					var downContent = $('<div></div>').addClass('down-content');
+					var courseLink = $('<a></a>').addClass('h4').text(course.title);
+					var courseDescription = $('<p></p>').text(course.description);
+					var button = $('<div></div>').addClass('text-button-pay').html('<a href="' + course.buttonLink + '">' + course.buttonText + '</a>');
+					/* var enrollLink = $('<a></a>').addClass('text-button-pay a').attr('href', '#' + course.sectionId).text(course.buttonText); */
+
+					/* downContent.append(courseLink, courseDescription, enrollLink); */
+
+					var contentList = $('<ul>');
+					$.each(course.courseContent, function(_index, content) {
+						var contentItem = $('<li>').addClass('course-ul').text(content);
+						contentList.append(contentItem);
+					});
+					
+					var enrollLink = $('<a></a>').addClass('text-button-pay a').attr('href', '#' + course.sectionId).text(course.buttonText);
+
+					downContent.append(courseLink, courseDescription, contentList, enrollLink);
+					carouselItem.append(img, downContent);
+					courseCarousel.append(carouselItem);
+				});
+
+				courseCarousel.owlCarousel({
+					loop: true,
+					margin: 10,
+					nav: true,
+					responsive: {
+						0: {
+							items: 1
+						},
+						594: {
+							items: 2
+						},
+						750: {
+							items: 2
+						},
+						890:{
+							items: 3
+						}
+					}
+				});
+			},
+			error: function(error) {
+				console.log('Error:', error);
+			}
+		});
+	});
+
+
+	  $(document).ready(function() {
+		$('#contact').submit(function(e) {
+		  e.preventDefault(); // Prevent the default form submission
+	  
+		  // Validate the form fields
+		  var name = $('#name').val();
+		  var email = $('#email').val();
+		  var phoneNumber = $('#phone-number').val();
+	  
+		  if (name === '' || email === '' || phoneNumber === '') {
+			// Display an error message or perform any other necessary validation
+			alert('Please fill out all fields');
+			return;
+		  }
+	  
+		  // Get the title and message to display in the modal
+		  var title = "Felicitaciones!"; // Custom title for the modal
+		  var message = "¡Registro exitoso! Gracias por unirte a nosotros. Te hemos enviado un correo de confirmación. Por favor, mantente al tanto en tu bandeja de entrada y verifica tu correo electrónico para completar el proceso de registro. Si no encuentras el correo en tu bandeja de entrada, asegúrate de revisar la carpeta de spam o correo no deseado. Si tienes alguna pregunta, no dudes en contactarnos.";
+	  
+		  // Update the modal title and body with the new content
+		  $('#myModal .modal-title').text(title);
+		  $('#myModal .modal-body p').text(message);
+	  
+		  // Display the modal
+		  $('#myModal').modal('show');
+		});
+	});
+
+	$(document).ready(function() {
+		$('#contact-end').submit(function(e) {
+		  e.preventDefault(); // Prevent the default form submission
+	  
+		  // Validate the form fields
+		  var name = $('#name').val();
+		  var email = $('#email').val();
+		  var phoneNumber = $('#phone-number').val();
+	  
+		  if (name === '' || email === '' || phoneNumber === '') {
+			// Display an error message or perform any other necessary validation
+			alert('Please fill out all fields');
+			return;
+		  }
+	  
+		  // Get the title and message to display in the modal
+		  var title = "¡Gracias por contactarnos! "; // Custom title for the modal
+		  var message = "Hemos recibido tu mensaje y nos pondremos en contacto contigo lo antes posible. Tu consulta es importante para nosotros y haremos todo lo posible por brindarte una respuesta oportuna.";
+	  
+		  // Update the modal title and body with the new content
+		  $('#myModal .modal-title').text(title);
+		  $('#myModal .modal-body p').text(message);
+	  
+		  // Display the modal
+		  $('#myModal').modal('show');
+		});
+	});
 
 
 	$("div.features-post").hover(
