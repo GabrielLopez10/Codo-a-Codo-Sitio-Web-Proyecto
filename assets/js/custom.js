a626a6cfd1fdae0dfac5815416df44a9582c89d1
Gaston Lopez <gastonlopez436@gmail.com>
2023-05-26 23:24:08 +0000

    Actualizacion 7

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,6 +11,67 @@
 	  return false;
 	});});
 
+	$(document).ready(function() {
+		// Handle tab clicks
+		$('#tabs ul li a').click(function(e) {
+		  e.preventDefault();
+		  var target = $(this).attr('href');
+		  $('html, body').animate({
+			scrollTop: $(target).offset().top
+		  }, 500);
+		});
+	  });
+
+//Navegacion por el Sitio
+$('.nav li:first').addClass('active');
+
+var showSection = function showSection(section, isAnimate) {
+  var
+  direction = section.replace(/#/, ''),
+  reqSection = $('.section').filter('[data-section="' + direction + '"]'),
+  reqSectionPos = reqSection.offset().top - 0;
+
+  if (isAnimate) {
+	$('body, html').animate({
+	  scrollTop: reqSectionPos },
+	800);
+  } else {
+	$('body, html').scrollTop(reqSectionPos);
+  }
+
+};
+
+var checkSection = function checkSection() {
+  $('.section').each(function () {
+	var
+	$this = $(this),
+	topEdge = $this.offset().top - 80,
+	bottomEdge = topEdge + $this.height(),
+	wScroll = $(window).scrollTop();
+	if (topEdge < wScroll && bottomEdge > wScroll) {
+	  var
+	  currentId = $this.data('section'),
+	  reqLink = $('a').filter('[href*=\\#' + currentId + ']');
+	  reqLink.closest('li').addClass('active').
+	  siblings().removeClass('active');
+	}
+  });
+};
+/* Al hacer scroll hacia las distintas secciones */
+$('.main-menu, .scroll-to-section').on('click', 'a', function (e) {
+  if($(e.target).hasClass('external')) {
+	return;
+  }
+  e.preventDefault();
+  $('#menu').removeClass('active');
+  showSection($(this).attr('href'), true);
+});
+
+$(window).scroll(function () {
+  checkSection();
+});
+
+// API Cursos
 
 	$(document).ready(function() {
 	    $.ajax({
@@ -74,6 +135,7 @@
 		});
 	});
 
+// POPUP Mensaje
 
 	  $(document).ready(function() {
 		$('#contact').submit(function(e) {
